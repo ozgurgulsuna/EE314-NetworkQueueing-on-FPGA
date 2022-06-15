e12a555b5416ad952346773ac9d0078ba0d3f016
ozgur <ozgurgulsuna@gmail.com>
2022-06-15 21:01:06 +0300

    drop --> drop per input

--- 2.Simulation/Quality of Service (QoS) Simulator/simulation_qos_v6.m
+++ 2.Simulation/Quality of Service (QoS) Simulator/simulation_qos_v6.m
@@ -1,25 +1,23 @@
-%% Quality of Service (QoS) Simulator
-% TO DO list 
-    % pack loss say
-%     % paket süresi tut
-% Wl=[0.8 0.4 0.2 0.1];          % Latency Weight Constant  
-% Wr=[10 20 30 40];          % Reliability Weight Constant
-%%
+%% Quality of Service (QoS) Simulator %%
+
 clear all;
 clc ; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+T_sample_size = 80000;      % Simulation Duration in Seconds
+T_in_step_per_sec = 4;      % Simulation time step is 250 ms
+T_time = 0;                 % Simulation time
+T_out = 3 ;                 % Output Data Period 5 Seconds
+T_in_mean = 3;              % Input Data Mean Period 3 Seconds
+T_in_jitter = 2.5;          % Input Data Jitter ±3 Seconds 
+
+Wl=[0.9 0.4 0.22 0.1];      % Latency Weight Constant  
+Wr=1.8*[3 6 8 10];              % Reliability Weight Constant
+k = 4;                      % Drop penalty
+% X = Wl.*t+Wr.*(f.^2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 counter = 0;
 
-T_sample_size = 20000;  % Simulation Duration in Seconds
-T_in_step_per_sec = 4; % Simulation time step is 250 ms
-T_time = 0;            % Simulation time
-T_out = 3 ;             % Output Data Period 5 Seconds
-T_in_mean = 3;         % Input Data Mean Period 3 Seconds
-T_in_jitter = 2.5;      % Input Data Jitter ±3 Seconds 
-
-% weights
-Wl=[0.80 0.4 0.2 0.1];          % Latency Weight Constant  
-Wr=[10 20 30 40];          % Reliability Weight Constant
 
 buffer_fullness=[0 0 0 0];  % buffer init
 
@@ -46,8 +44,9 @@ T_time_array=linspace(0.25,T_sample_size,T_sample_size*T_in_step_per_sec);
 
 % Period Randomization
 for i=1:T_sample_size*T_in_step_per_sec/2
-    s(i)=round((T_in_jitter*T_in_step_per_sec)*(sin(i/10))+(T_in_mean*T_in_step_per_sec));
-    r(i)=round((T_in_mean*T_in_step_per_sec-T_in_jitter*T_in_step_per_sec+T_in_mean*T_in_step_per_sec)*randi([0,1])+(T_in_mean*T_in_step_per_sec-T_in_jitter*T_in_step_per_sec));
+    r(i)=round((T_in_jitter*T_in_step_per_sec)*(sin(i/10))+(T_in_mean*T_in_step_per_sec));
+    s(i)=round((T_in_jitter*T_in_step_per_sec)*(randi([-1,1]))+(T_in_mean*T_in_step_per_sec));
+    %r(i)=round((T_in_mean*T_in_step_per_sec-T_in_jitter*T_in_step_per_sec+T_in_mean*T_in_step_per_sec)*randi([0,1])+(T_in_mean*T_in_step_per_sec-T_in_jitter*T_in_step_per_sec));
 end
 
 % figure;
@@ -79,7 +78,7 @@ for i=1:length(T_time_array)
             if buffer_fullness(1)==6
                 d(1) = d(1)+1;  
                 f(1)=buffer_fullness(1);
-                T(1)=T(1)+2*max(all_data(:,1)); 
+                T(1)=T(1)+k*max(all_data(:,1)); 
                 all_data(find(all_data(:,1)==max(all_data(:,1)),1,"first"),1)=1; % remove the oldest data
             else
                 buffer_fullness(1) = buffer_fullness(1)+1;
@@ -91,7 +90,7 @@ for i=1:length(T_time_array)
             if buffer_fullness(2)==6
                 d(2) = d(2)+1; 
                 f(2)=buffer_fullness(2);
-                T(2)=T(2)+2*max(all_data(:,2)); 
+                T(2)=T(2)+k*max(all_data(:,2)); 
                 all_data(find(all_data(:,2)==max(all_data(:,2)),1,"first"),2)=1; % remove the oldest data
 
                 % do nothing
@@ -105,7 +104,7 @@ for i=1:length(T_time_array)
             if buffer_fullness(3)==6
                 d(3) = d(3)+1; 
                 f(3)=buffer_fullness(3);
-                T(3)=T(3)+2*max(all_data(:,3)); 
+                T(3)=T(3)+k*max(all_data(:,3)); 
                 all_data(find(all_data(:,3)==max(all_data(:,3)),1,"first"),3)=1; % remove the oldest data
                 % do nothing
             else
@@ -119,7 +118,7 @@ for i=1:length(T_time_array)
             if buffer_fullness(4)==6
                 d(4) = d(4)+1; 
                 f(4)=buffer_fullness(4);
-                T(4)=T(4)+2*max(all_data(:,4)); 
+                T(4)=T(4)+k*max(all_data(:,4)); 
                 all_data(find(all_data(:,4)==max(all_data(:,4)),1,"first"),4)=1; % remove the oldest data
                 % do nothing
             else
@@ -130,7 +129,7 @@ for i=1:length(T_time_array)
             end
         otherwise
     end
-    X=Wl.*t+Wr.*f; % Evaluate buffer priority
+    X=Wl.*t+Wr.*(f.^2); % Evaluate buffer priority
     if mod(i,T_out*T_in_step_per_sec)==0  % its time to get the output
         switch find(X==(max(X)),1,"last")             % burada iki ya da daha fazla aynı değerde max varsa ilkini alıyoruz ama bence sonuncusunu almalıyız
             case 1
@@ -196,7 +195,8 @@ all_data=all_data+ones(s_all_data(1),s_all_data(2)).*fillmissing((all_data./all_
 all_data;
 end
 
-a=T./read_count;
+d_pi=d./(read_count+d);  % drop per input
+a=T./(read_count+d);     % average latency per output
 
 % 4.62536598665238	2.64147390882448	2.27016506027572	1.66617193587623	2.81002645322608	-1.82470119865187	2.69544383651092	2.54335164446693
 
