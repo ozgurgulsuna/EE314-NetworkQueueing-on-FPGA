361c837975b29ea7d0049cad51f5f6958ee61fe8
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2022-07-03 13:32:37 +0300

    [Firmware] Extensive test on buffer

--- 3.Firmware/Decision_Test/simulation/qsim/work/decision_test/_primary.vhd
+++ 3.Firmware/Decision_Test/simulation/qsim/work/decision_test/_primary.vhd
@@ -9,6 +9,10 @@ entity decision_test is
         pin             : in     vl_logic_vector(3 downto 0);
         full2           : out    vl_logic_vector(2 downto 0);
         full3           : out    vl_logic_vector(2 downto 0);
-        full4           : out    vl_logic_vector(2 downto 0)
+        full4           : out    vl_logic_vector(2 downto 0);
+        MEM_1           : out    vl_logic_vector(23 downto 0);
+        MEM_2           : out    vl_logic_vector(23 downto 0);
+        MEM_3           : out    vl_logic_vector(23 downto 0);
+        MEM_4           : out    vl_logic_vector(23 downto 0)
     );
 end decision_test;
--- 3.Firmware/Decision_Test/simulation/qsim/work/decision_test_vlg_check_tst/_primary.vhd
+++ 3.Firmware/Decision_Test/simulation/qsim/work/decision_test_vlg_check_tst/_primary.vhd
@@ -6,6 +6,10 @@ entity decision_test_vlg_check_tst is
         full2           : in     vl_logic_vector(2 downto 0);
         full3           : in     vl_logic_vector(2 downto 0);
         full4           : in     vl_logic_vector(2 downto 0);
+        MEM_1           : in     vl_logic_vector(23 downto 0);
+        MEM_2           : in     vl_logic_vector(23 downto 0);
+        MEM_3           : in     vl_logic_vector(23 downto 0);
+        MEM_4           : in     vl_logic_vector(23 downto 0);
         sampler_rx      : in     vl_logic
     );
 end decision_test_vlg_check_tst;
