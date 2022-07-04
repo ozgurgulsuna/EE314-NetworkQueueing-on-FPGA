409d8fce3bd612e171f2cf244809e3155c8ef040
Işık <emir.altunkol01@gmail.com>
2022-07-04 18:42:25 +0300

    [Firmware] Finally it's time to sleep
    Fuck Verilog

--- 3.Firmware/Verilog_Outlines/debouncer.v
+++ 3.Firmware/Verilog_Outlines/debouncer.v
@@ -1,17 +1,18 @@
 module debouncer(
 	input pb_1,
-	input clk,
+	input fract_clk,
 	output pb_out
 );
 
 	wire Q1,Q2,Q2_bar,Q0;
 
-	d_ff d0(clk, pb_1,Q0 );
-	d_ff d1(clk, Q0,Q1 );
-	d_ff d2(clk, Q1,Q2 );
+	d_ff d0(fract_clk, pb_1,Q0 );
+	d_ff d1(fract_clk, Q0,Q1 );
+	d_ff d2(fract_clk, Q1,Q2 );
 	
 	assign Q2_bar = ~Q2;
 	assign pb_out = Q1 & Q2_bar;
+	
 endmodule
 
 module d_ff(input DFF_CLOCK, D, output reg Q);
